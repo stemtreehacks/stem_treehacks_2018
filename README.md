34868afb91b62dba6c16fe0da569aab3058318ca
chzozu <chzou2009@gmail.com>
2018-02-18 05:36:43 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # energy-hacks
+
+To set up: `$ npm install`
+
+Name your prediction files `pred1.csv` and `pred2.csv` if you want battery thresholds
--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { 
       num_graphs: 1, 
       labels: [[]], 
-      points: [[]], 
+      points1: [[]], 
+      points2: [], 
       saved:  [],
-      lines:  [[]],
+      lines:  [],
       titles: ['Your Data Here'] 
   });
 });
@@ -45,6 +46,7 @@ router.post('/', function(req, res, next) {
   var upload_path = __dirname.replace('/routes', '/uploads/');
   var csv_path = "";
   var split_by = "month";
+  var threshold = 0;
   form.parse(req);
 
   // Form handlers
@@ -61,40 +63,54 @@ router.post('/', function(req, res, next) {
       }
   });
   form.on('end', function() {
+      if (csv_path.includes('pred1.csv')) {
+          threshold = 362;
+      } else if (csv_path.includes('pred2.csv')) {
+          threshold = 300; // TODO
+      }
+      display_data(csv_path);
+      /*
       var pythonProcess = spawn('python', ['dummy.py', csv_path]);
       pythonProcess.stdout.on('data', function(data) {
           new_path = data.toString().trim();
           console.log(new_path);
           display_data(new_path);
       });
+      */
   });
 
   // Function called to display data from csv file
   var display_data = (load_path) => {
-      var labels = []; var points = []; var saveds = [];
+      var labels = []; var points1 = []; var points2 = []; var saveds = [];
       fs.createReadStream(load_path) // Read local csv file
         .pipe(csv())
         .on('data', function(data) {
             var row = Object.keys(data);
-            if (row.length == 3) {
+            if (row.length == 4) {
+                [label, point1, point2, saved] = row;
+                labels.push(data[label]);
+                points1.push(parseFloat(data[point1]));
+                points2.push(parseFloat(data[point2]));
+                saveds.push(parseFloat(data[saved]));
+            } else if (row.length == 3) {
                 [label, point, saved] = row;
                 labels.push(data[label]);
-                points.push(parseFloat(data[point]));
+                points1.push(parseFloat(data[point]));
                 saveds.push(parseFloat(data[saved]));
             } else {
                 [label, point] = row;
                 labels.push(data[label]);
-                points.push(parseFloat(data[point]));
+                points1.push(parseFloat(data[point]));
             }
         })
         .on('end', function() {
             // Make chart from data
             if (split_by === 'year') {
-                lib.cherry.treehax['@dev'].split_year(labels, points, saveds, split_callback);
+                lib.cherry.treehax['@dev'].split_year(labels, points1, points2, saveds, split_callback);
             } else if (split_by === 'month') {
-                lib.cherry.treehax['@dev'].split_month(labels, points, saveds, split_callback);
+                lib.cherry.treehax['@dev'].split_month(labels, points1, points2, saveds, split_callback);
             } else {
-                lib.cherry.treehax['@dev'].split_day(labels, points, saveds, split_callback);
+                lib.cherry.treehax['@dev'].split_day(labels, points1, points2, saveds, split_callback);
             }
         });
   };
@@ -103,11 +119,15 @@ router.post('/', function(req, res, next) {
       if (err) {
           console.log(err);
       } else {
-          var lines = generate_lines(result.labels, 338);
+          var lines = [];
+          if (threshold > 0) {
+              lines = generate_lines(result.labels, threshold);
+          }
           res.render('index', { 
               num_graphs: result.count,
               labels: result.labels, 
-              points: result.points,
+              points1: result.points1,
+              points2: result.points2,
               saved:  result.saved,
               lines:  lines,
               titles: result.titles
